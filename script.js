3a65a32157641bbaee595bdf5617154f8a9544d6
Angeline Marcelle Lukito <87764018+angelinemarcelle@users.noreply.github.com>
2023-03-30 16:44:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ buttons.forEach(button => {
     button.addEventListener('click', () => {
         const value = button.textContent;
 
-        if (value === 'C') {
+        if (value === 'AC') {
             display.value = '';
         } else if (value === '=') {
             try {
@@ -13,6 +13,10 @@ buttons.forEach(button => {
             } catch (error) {
                 display.value = 'Error';
             }
+        } else if (value === '+/-') {
+            display.value = -parseFloat(display.value);
+        } else if (value === '%') {
+            display.value = parseFloat(display.value) / 100;
         } else {
             display.value += value;
         }
